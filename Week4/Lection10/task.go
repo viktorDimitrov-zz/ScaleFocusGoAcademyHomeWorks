ea242edcf11dcb6fa420fb13fa18f7cc79d1f3f2
Viktor Dimitrov <viktor_dimitrov@abv.bg>
2022-03-16 12:28:04 +0200

    fulfit Lection10 task

--- Week4/Lection10/task.go
+++ Week4/Lection10/task.go
@@ -1,7 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
-func main(){
+type ConcurrentPrinter struct {
+	sync.WaitGroup
+	sync.Mutex
+	counter     int
+	lastElement string
+}
 
+func (cp *ConcurrentPrinter) printFoo(times int) {
+	/* provide your implementation */
+	cp.Add(1)
+	go func() {
+		defer cp.Done()
+
+		for {
+			cp.Lock()
+			if cp.counter == times {
+				cp.Unlock()
+				break
+			}
+			if cp.lastElement != "foo" {
+				fmt.Print("foo")
+				cp.lastElement = "foo"
+				cp.counter++
+			}
+			cp.Unlock()
+			time.Sleep(10 * time.Microsecond)
+		}
+	}()
+}
+
+func (cp *ConcurrentPrinter) printBar(times int) {
+	/* provide your implementation */
+	cp.Add(1)
+	go func() {
+		defer cp.Done()
+
+		for {
+			cp.Lock()
+			if cp.counter == times {
+				cp.Unlock()
+				break
+			}
+			if cp.lastElement != "bar" {
+				fmt.Print("bar")
+				cp.lastElement = "bar"
+				cp.counter++
+			}
+			cp.Unlock()
+			time.Sleep(10 * time.Microsecond)
+		}
+	}()
+}
+
+func main() {
+	times := 10
+	cp := &ConcurrentPrinter{}
+	cp.printFoo(times)
+	cp.printBar(times)
+	cp.Wait()
 }
