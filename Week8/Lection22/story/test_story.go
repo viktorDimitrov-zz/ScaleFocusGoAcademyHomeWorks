e942398417d7bb355507e4987d47115e02d08d32
Viktor Dimitrov <viktor_dimitrov@abv.bg>
2022-05-09 15:15:23 +0300

    added tests

--- Week8/Lection22/story/test_story.go
+++ Week8/Lection22/story/test_story.go
@@ -6,8 +6,6 @@ import (
 	"net/http/httptest"
 	"reflect"
 	"testing"
-
-//	"github.com/stretchr/testify/assert"
 )
 
 func handleTopStories(ids []int) http.HandlerFunc {
@@ -15,18 +13,46 @@ func handleTopStories(ids []int) http.HandlerFunc {
 		json.NewEncoder(w).Encode(ids)
 	}
 }
+
+func handleGetStory(stories []Story) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		//try to find the id of querry
+		var id int
+		var resultStory Story
+
+		for _, v := range stories {
+			if v.Id == id {
+				resultStory = v
+				break
+			}
+		}
+		//return story
+		json.NewEncoder(w).Encode(resultStory)
+	}
+}
+
 func TestTopStoriesIds(t *testing.T) {
 	router := http.NewServeMux()
 	ids := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	stories := []Story{
+		{
+			Id:    10,
+			Title: "test 10",
+			Score: 15,
+		},
+	}
+
 	router.Handle("/v0/topstories.json", handleTopStories(ids))
+	router.Handle("/v0/item", handleGetStory(stories))
 	mockServer := httptest.NewServer(router)
 
-	wont:=ids[:10]
+	wont := stories
 	//Act
 	ss := NewStoryService(mockServer.URL)
-	got:=ss.GetTopStoriesIds()
+	got := ss.GetStoriesByIds(ids)
+
 	//Asssert
-	if !reflect.DeepEqual(got,wont){
-		t.Fatalf("Get %v,want %v",got,wont)
+	if !reflect.DeepEqual(got, wont) {
+		t.Fatalf("Get %v,want %v", got, wont)
 	}
 }
